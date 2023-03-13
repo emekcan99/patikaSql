7f9d9bbb48a526508ce9bf471624960eb6bb406e
emekcan99 <94544627+emekcan99@users.noreply.github.com>
2023-03-13 21:11:06 +0300

    Create ödev-12.sql

---
+++ ödev-12.sql
@@ -0,0 +1,27 @@
+--film tablosunda film uzunluğu length sütununda gösterilmektedir. Uzunluğu ortalama film uzunluğundan fazla kaç tane film vardır?
+
+SELECT COUNT(*) FROM film
+WHERE length > 
+(SELECT AVG(length) FROM film);
+
+--film tablosunda en yüksek rental_rate değerine sahip kaç tane film vardır?
+
+SELECT COUNT(*) FROM film
+WHERE rental_rate = 
+(SELECT MAX(rental_rate) FROM film);
+
+
+--film tablosunda en düşük rental_rate ve en düşün replacement_cost değerlerine sahip filmleri sıralayınız.
+
+SELECT * FROM film
+WHERE rental_rate = (SELECT MIN(rental_rate) FROM film) 
+AND replacement_cost = (SELECT MIN(replacement_cost) FROM film);
+
+--payment tablosunda en fazla sayıda alışveriş yapan müşterileri(customer) sıralayınız.
+
+
+SELECT first_name, last_name, COUNT(customer.customer_id) FROM payment
+JOIN customer 
+ON payment.customer_id = customer.customer_id
+GROUP BY payment.customer_id, customer.first_name, customer.last_name
+ORDER BY count DESC;
